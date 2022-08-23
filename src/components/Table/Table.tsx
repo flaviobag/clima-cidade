9dd1fa47d3a9f6257ffbc372d6ec10a11830659d
flaviobag <92397080+flaviobag@users.noreply.github.com>
2022-08-23 09:49:31 -0300

    tentaiva numero mil

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,44 +1,17 @@
 import React from 'react';
+import { createPartiallyEmittedExpression } from 'typescript';
+import { cachedDataVersionTag } from 'v8';
+import { Title } from '../../pages/Home/Home.styles';
 import { Table, TableTitle } from './Table.styles';
 
 const TableCidade = () => (
   <>
-    <TableTitle>Capitais</TableTitle>
-    <Table>
-      <thead>
-        <tr>
-          <th>Min</th>
-          <th>Max</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>12º</td>
-          <td>25º</td>
-          <td>Sao Paulo</td>
-        </tr>
-        <tr>
-          <td>12º</td>
-          <td>25º</td>
-          <td>Rio de Janeiro</td>
-        </tr>
-        <tr>
-          <td>12º</td>
-          <td>25º</td>
-          <td>Belo Horizonte</td>
-        </tr>
-        <tr>
-          <td>12º</td>
-          <td>25º</td>
-          <td>Brasilia</td>
-        </tr>
-        <tr>
-          <td>12º</td>
-          <td>25º</td>
-          <td>Belém</td>
-        </tr>
-      </tbody>
-    </Table>
+    
+    <Title>Capitais</Title>
+      <TableWrapper>
+        <Table capitais={capitais.slice(0, halfLength)} />
+        <Table capitais={capitais.slice(halfLength, capitais.length)} />
+      </TableWrapper>
   </>
 );
 
